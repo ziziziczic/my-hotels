22c82aa84c072fb1e0379cb20aa353b743f2d410
gohayng <hometown1129@tidesquare.com>
2025-06-03 16:28:59 +0900

    feat: 작업환경 셋팅하다

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */ // JSDoc을 사용하여 타입 힌트를 얻을 수 있습니다.
+const nextConfig = {
+  // ... 설정 내용
+  reactStrictMode: true,
+  // ...
+};
+
+module.exports = nextConfig; // CommonJS 방식으로 내보내기 (가장 일반적)
