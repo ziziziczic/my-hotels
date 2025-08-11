9a52e57dcf0b1d3e70eb22ec0ce92617054de6cc
gohayng <hometown1129@tidesquare.com>
2025-08-11 23:57:10 +0900

    feat : zustand 로 더하기 빼기를 구현하다

--- app/(pc)/PcPage.tsx
+++ app/(pc)/PcPage.tsx
@@ -3,13 +3,15 @@
 import { hotelItem } from '@/app/api/hotelList/route';
 import SearchForm from '../_components/SearchForm';
 import { useState } from 'react';
-import { ReviewContextProvider } from '../_provider/useReview';
+import { ReviewContextProvider } from '../_store/useReviewStore';
 import Review from '../_components/review';
 import Sub from '../_components/sub';
+import { useCounterStore } from '../_store/useCounterStore';
 // import Tabs from './_components/Tabs';
 
 const PcPage = ({ hotelList }: { hotelList: hotelItem[] }) => {
   const [hotels, setHotels] = useState(hotelList);
+  const { count, increment, decrement } = useCounterStore();
 
   return (
     <div>
@@ -22,6 +24,11 @@ const PcPage = ({ hotelList }: { hotelList: hotelItem[] }) => {
         </Review>
       </ReviewContextProvider>
       {/* <Tabs /> */}
+      <div className="flex gap-2">
+        <span>count : {count}</span>
+        <button onClick={increment}>더하기</button>
+        <button onClick={decrement}>빼기</button>
+      </div>
     </div>
   );
 };
